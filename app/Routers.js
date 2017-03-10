6a2c8d84f8e28c72d17997650ab30a0e25140c63
mlkiung <meredith.kiung@gmail.com>
2017-03-10 11:54:31 -0500

    saved

--- app/Routers.js
+++ app/Routers.js
@@ -1,12 +1,16 @@
-import React from 'react'
-import { Scene, Router } from 'react-native-router-flux'
+import React from 'react';
+import { Scene, Router } from 'react-native-router-flux';
 
 import Welcome from './components/Welcome/Welcome';
+import GridRestaurants from './components/Restaurants/GridRestaurantsAllView';
+import EntryPage from './components/EntryPage/EntryPage';
 
 const RouterComponent = () => {
     return (
         <Router>
             <Scene key="welcome" component={Welcome} title="Welcome" initial />
+            <Scene key="restaurants" component={GridRestaurants} title="Past Eats" />
+            <Scene key="entry" component={EntryPage} title="RESTAURANT NAME" />
         </Router>
     )
 }
