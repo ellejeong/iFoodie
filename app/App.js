6a24fb7cf79ce3618e085d7c39a38783f04c23a9
mlkiung <meredith.kiung@gmail.com>
2017-03-11 18:39:22 -0500

    meh

--- app/App.js
+++ app/App.js
@@ -13,13 +13,7 @@ const store = createStore(reducers, {}, applyMiddleware(thunkMiddleware));
 class App extends Component {
 
 	componentWillMount() {
-		firebase.initializeApp({
-	apiKey: 'AIzaSyBwcy1m9XRqLYAINcyKObdaiVP2XH2BadY',
-	authDomain: 'ifoodie-4728f.firebaseapp.com',
-	databaseURL: 'https://ifoodie-4728f.firebaseio.com',
-	storageBucket: 'ifoodie-4728f.appspot.com',
-	messagingSenderId: '694524282354'
-});
+		firebase.initializeApp(firebaseInfo);
 	}
 
 	render() {
