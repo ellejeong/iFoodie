0a69e06a6f5ddc6ae96ae50604385405dd199c94
Laura Chung <3llejeong@gmail.com>
2017-03-12 16:00:10 -0400

    about to push to master

--- app/components/EntryPage/EntryForm.js
+++ app/components/EntryPage/EntryForm.js
@@ -116,35 +116,14 @@ render() {
 
         <View style={styles.dishContainer}>
             <Text style={styles.dish}>Dish Name: </Text>
-      <GooglePlacesAutocomplete
-            placeholder='Enter Location'
-            minLength={2}
-            autoFocus={false}
-            fetchDetails={true}
-            query={{
-          // available options: https://developers.google.com/places/web-service/autocomplete
-                key: 'AIzaSyBX24ClG46CmZeN9iSOu9tCKJnljh9b09Q',
-                language: 'en', // language of the results
-                types: '(cities)', // default: 'geocode'
-            }}
-            styles={{
-                    textInputContainer: {
-                        backgroundColor: 'rgba(0,0,0,0)',
-                        borderTopWidth: 0,
-                        borderBottomWidth:0
-                    },
-                    textInput: {
-                        marginLeft: 0,
-                        marginRight: 0,
-                        height: 38,
-                        color: '#5d5d5d',
-                        fontSize: 16
-                    },
-                    predefinedPlacesDescription: {
-                        color: '#1faadb'
-                    },
-            }}
-            currentLocation={false}/>
+                        <TextInput
+                    onChangeText={this.onDishChange}
+                    style={styles.dishInput}
+                    placeholder="Dish Name"
+                    placeholderTextColor='#9cd19d'
+                    returnKeyType= "done"
+                    autoCorrect={false}
+                    value={this.state.dish}/>
         </View>
 
         <View style={styles.coursesContainer}>
